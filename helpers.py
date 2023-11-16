a5c80c58b06ffbaf074fd5b5ee71ab4ae64ef0cc
eliamounier <elia.mounier-poulat@epfl.ch>
2023-11-16 23:28:31 +0100

    cleaned

--- helpers.py
+++ helpers.py
@@ -15,18 +15,25 @@ import seaborn as sns
 
 
 def compute_nan_count_and_percentage(df):
+    """
+    Given a DataFrame, print the NaN count per column.
+    """
+
     for column_name in df.columns:
         nan_count = df[column_name].isna().sum()
         nan_percentage = df[column_name].isna().mean() * 100
 
         print(
-            f"{column_name}: {nan_count} NaN values, which represents {nan_percentage:.2f} % of the column. \n"
+            f"{column_name}: {nan_count} NaN values, which represents {nan_percentage:.2f} % of the column.\n"
         )
 
 
 def find_region(country_or_countries, regions):
+    """
+    Find the region(s) to which the given country or list of countries belong.
+    """
+
     if isinstance(country_or_countries, list):
-        # since the country is stored as a list and there might be multiple so we have to check them too
         for country in country_or_countries:
             for region, countries in regions.items():
                 if country in countries:
@@ -34,30 +41,35 @@ def find_region(country_or_countries, regions):
     return None
 
 
-# Function to match movies to a specific event based on the threshold
-
-
 def categorize_event(summary, event_dict, threshold):
-    # Remove punctuation and set all words to lower case
+    """
+    Categorize a movie based on the presence of specific words associated with an event.
+
+    Parameters:
+    - summary (str): The plot summary of the movie.
+    - event_dict (str): A regular expression pattern representing words related to the event.
+    - threshold (int): The minimum number of common words required for categorization.
+
+    Returns:
+    - bool: True if the movie is categorized based on the threshold, False otherwise.
+    """
+
     summary_no_punct = remove_punctuation(summary)
     summary_words = summary_no_punct.lower()
-    
-    # Define your search pattern
-    pattern = re.compile(event_dict)
 
-    # Search for the pattern in the text
+    pattern = re.compile(event_dict)
     matches = re.findall(pattern, summary_words)
-    
+
     common_words_count = len(matches)
-                                         
+
     return common_words_count >= threshold
 
 
-# Function to add columns to DataFrame based on event categories
-
-
 def add_event_columns(df, dictionaries_df):
-    # Iterate over each event in dictionaries_df and add a column to df
+    """
+    Add columns to a DataFrame based on event categories.
+    """
+
     for event, row in dictionaries_df.iterrows():
         event_words = row["dictionaries"]
         threshold = row["threshold"]
@@ -68,16 +80,22 @@ def add_event_columns(df, dictionaries_df):
 
 
 def create_events_belongs_to_column(df, event_columns):
+    """
+    Create an 'events_belongs_to' column in the DataFrame based indicating the events to which each movie belongs.
+    """
+
     df["events_belongs_to"] = df.apply(
         lambda row: [event for event in event_columns if row[event]], axis=1
     )
     return df
 
 
-# Function to add one column to DataFrame which will be filled by a list of genre categories
-
-
 def update_new_genre_column(df, genres_considered):
+    """
+    Add a 'new_genre' column to the DataFrame, indicating the
+    corresponding genre categories for each movie.
+    """
+
     df["new_genre"] = None
 
     for name, genre_list in genres_considered.items():
@@ -93,20 +111,24 @@ def update_new_genre_column(df, genres_considered):
 
 
 def count_genres_for_event(df, event_column):
-    # Explode the 'new_genre' column
+    """
+    Count the occurrences of genres for movies belonging to a specific event.
+    """
     df_expanded = df.copy()
     df_expanded = df_expanded.explode("new_genre")
 
-    # Filter rows where the event_column is True
     filtered_df = df_expanded[df_expanded[event_column] == 1]
 
-    # Group by 'new_genre' and count occurrences
     genre_counts = filtered_df.groupby("new_genre").size()
 
     return genre_counts
 
 
 def calculate_genre_proportions_by_event(df):
+    """
+    Calculate genre proportions for each event based on the provided DataFrame.
+    """
+
     genre_proportions_by_event = pd.DataFrame()
 
     for event in df["events_belongs_to"].explode().unique():
@@ -121,112 +143,137 @@ def calculate_genre_proportions_by_event(df):
     return genre_proportions_by_event
 
 
-# Function that allows to remove punctuation in a string, we will use it in th eword search process
-
 def remove_punctuation(input_string):
-    # Create a translation table to map punctuation characters to None
+    """
+    Remove punctuation characters from the given input string.
+    """
+
     translation_table = str.maketrans("", "", string.punctuation)
 
-    # Use translate to remove punctuation
     result_string = input_string.translate(translation_table)
 
     return result_string
 
 
-# Function to verify if the movies were correctly assigned to their corresponding event(s) 
-# Checks whether the true event/ movement to which the movie corresponds is also a dictionnary-mapped event/movement 
-
 def check_matching(row):
-    return row['true_event'] in row['events_belongs_to']
+    """
+    Check if the movie was correctly assigned to its corresponding event(s).
+    """
+
+    return row["true_event"] in row["events_belongs_to"]
 
 
 def translations_imbd_to_freebase():
+    """
+    Retrieve IMDb to Freebase translations using SPARQL query on Wikidata.
 
-    url = 'https://query.wikidata.org/sparql'
+    Returns:
+    - pandas.DataFrame: DataFrame containing IMDb IDs and corresponding Freebase IDs.
+    """
+
+    url = "https://query.wikidata.org/sparql"
     query = """ SELECT ?item ?imdbID ?freebaseID WHERE {?item wdt:P345 ?imdbID.OPTIONAL {?item wdt:P646 ?freebaseID.}}"""
-    headers = {'User-Agent': 'WDQS-example Python/%s.%s' % (sys.version_info[0], sys.version_info[1])}
-    data = requests.get(url, headers=headers, params={'query': query, 'format': 'json'}).json()
+    headers = {
+        "User-Agent": "WDQS-example Python/%s.%s"
+        % (sys.version_info[0], sys.version_info[1])
+    }
+    data = requests.get(
+        url, headers=headers, params={"query": query, "format": "json"}
+    ).json()
 
-    imdb = [item['imdbID']['value'] for item in data['results']['bindings']]
-    freebase = [item.get('freebaseID', {}).get('value', None) for item in data['results']['bindings']]
+    imdb = [item["imdbID"]["value"] for item in data["results"]["bindings"]]
+    freebase = [
+        item.get("freebaseID", {}).get("value", None)
+        for item in data["results"]["bindings"]
+    ]
 
-    result_df = pd.DataFrame(data ={ 'imdb_id': imdb, 'id_freebase': freebase})
+    result_df = pd.DataFrame(data={"imdb_id": imdb, "id_freebase": freebase})
 
     return result_df
 
+
 def get_unique_countries(input_list):
+    """
+    Extract unique countries from a list.
+    """
+
     unique_countries = []
     for item in input_list:
         if item not in unique_countries:
             unique_countries.append(item)
     return unique_countries
 
-def calculate_false_positive_counts(id_list, movies_df, events_list):
-    # Initialize a dictionary to store false positive counts for each event
-    false_positive_counts = {event: 0 for event in events_list}
-
-    for element in id_list:
-        movie_to_test = movies_df.query('id_freebase == @element')
-        for event in events_list:
-            if list(movie_to_test[event])[0]:
-                false_positive_counts[event] += 1
-
-    return false_positive_counts
 
 def movie_affected_to_event(df, event):
+    """
+    Filter the DataFrame to include only rows where a specific 'event' is True.
+    """
+
     return df[df[event] != False]
 
+
 def plot_events_over_years(movies_events, events, figures_per_row=4):
-    # Extract relevant columns
-    columns_to_extract = ['date'] + events
+    """
+    Plot for each events, the number of movies over the years.
+    """
+
+    columns_to_extract = ["date"] + events
     events_df = movies_events[columns_to_extract]
 
-    # Calculate the number of rows needed
-    num_rows = -(-len(events) // figures_per_row)  # Ceiling division
+    num_rows = -(-len(events) // figures_per_row)
 
-    # Create subplots for each event
-    fig, axes = plt.subplots(num_rows, figures_per_row, figsize=(20, 5 * num_rows), sharex=True, sharey=True)
+    fig, axes = plt.subplots(
+        num_rows, figures_per_row, figsize=(20, 5 * num_rows), sharex=True, sharey=True
+    )
 
-    # Flatten the axes array if there is only one row
     axes = axes.flatten()
 
-    # Define a color palette for better distinction
-    palette = sns.color_palette('husl', n_colors=len(events))
+    palette = sns.color_palette("husl", n_colors=len(events))
 
-    # Loop through each event and plot on a separate subplot
     for i, event in enumerate(events):
         row_idx = i // figures_per_row
         col_idx = i % figures_per_row
 
-        # Filter data for the specific event
-        event_data = events_df[['date', event]]
+        event_data = events_df[["date", event]]
 
-        # Melt the DataFrame to have 'date' as the identifier variable
-        event_melted = pd.melt(event_data, id_vars=['date'], value_vars=[event], var_name='event', value_name='occurred')
+        event_melted = pd.melt(
+            event_data,
+            id_vars=["date"],
+            value_vars=[event],
+            var_name="event",
+            value_name="occurred",
+        )
 
-        # Filter only rows where the event occurred
-        event_occurred = event_melted[event_melted['occurred']]
+        event_occurred = event_melted[event_melted["occurred"]]
 
-        # Group by 'date' and count the occurrences
-        event_count = event_occurred.groupby('date').size().reset_index(name='count')
+        event_count = event_occurred.groupby("date").size().reset_index(name="count")
 
-        # Plot on the corresponding subplot with a unique color
-        sns.lineplot(x='date', y='count', data = event_count, ax=axes[i], label=event, color=palette[i])
+        sns.lineplot(
+            x="date",
+            y="count",
+            data=event_count,
+            ax=axes[i],
+            label=event,
+            color=palette[i],
+        )
 
-        # Set subplot title
-        axes[i].set_title(f'{event}')
+        axes[i].set_title(f"{event}")
 
-    # Remove empty subplots if there are any
     for i in range(len(events), len(axes)):
         fig.delaxes(axes[i])
 
     plt.tight_layout()
     plt.show()
 
+
 def get_year_percentage(df, event):
-    
+    """
+    Calculate the percentage of movies for a specific event over the years.
+    """
     event_data = movie_affected_to_event(df, event)
-    year_percentage_series = event_data.groupby('date').size() / df.groupby('date').size() * 100
+    year_percentage_series = (
+        event_data.groupby("date").size() / df.groupby("date").size() * 100
+    )
     year_percentage_series = year_percentage_series.fillna(0)
-    
+
     return year_percentage_series
